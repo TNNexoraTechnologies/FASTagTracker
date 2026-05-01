3c3978a391318914f4cf8ced3c1c05b6203b9d39
singhvaishu005@gmail.com <vaishnavi.@Vaishnavis-MacBook-Air.local>
2026-05-01 21:36:11 +0530

    user schema

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,70 @@
-export const loginUser = (req, res) => {
-  // logic
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+// ================= REGISTER =================
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// ================= LOGIN =================
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { registerUser, loginUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
-// routes here
+// const {
+//     registerUser,
+//     loginUser
+// } = require('../controllers/authController');
+
+// Routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
